b74babddecd7e6e9a7c6431fb19c02fa63995a30
Mohamed <Mohamed@1111wwe@gmail.com>
2025-08-24 12:06:09 +0300

    Q2

--- ExercisesOnLinq/Program.cs
+++ ExercisesOnLinq/Program.cs
@@ -13,12 +13,31 @@ namespace ExercisesOnLinq
             var OrderItem=Repository.LoadOrderItems();
             var Payments= Repository.LoadPayments();
             var Shipment=Repository.LoadShipments();
+
             #region Q1
             //1. From the orders, find the first order where the
             //`TotalAmount` is above the average total amount of all orders,
             //var result=Orders.FirstOrDefault(o=>o.TotalAmount > Orders.Average(o=>o.TotalAmount));
             //Repository.PrintList([result]);
 
+            #endregion
+
+            #region Q2
+            /*Retrieve all products in category "Electronics" with a price greater than 500,
+             * ordered by price descending, selecting only the product name and price.*/
+            //var result = Products.Join(Category,p=>p.CategoryId,c=>c.Id,
+            //    (p, c) => new
+            //    {
+            //        Product = p,
+            //        Category = c,
+
+            //    }).Where(res => res.Product.Price > 500 && res.Category.Name == "Electronics")
+            //    .OrderByDescending(res => res.Product.Price)
+            //    .Select(res => new { res.Product.Name, res.Product.Price });
+            // foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
             #endregion
         }
     }
